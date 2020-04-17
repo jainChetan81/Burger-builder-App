07e53b3e2a72ca46fc98d2b94df988384434d4ae
terminatorDX <jain.cj.chetan@gmail.com>
2020-04-17 14:26:56 +0530

    changes in structure 2

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Auxillary from "../Auxillary";
+import classes from "./Layout.css";
+import ToolBar from "../../components/Navigation/ToolBar/ToolBar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+
+export class Layout extends Component {
+    state = {
+        showSideDrawer: false,
+    };
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false,
+        });
+    };
+    sideDrawerToggleHandler = () => {
+        console.log("clicked");
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer,
+            };
+        });
+    };
+    render() {
+        console.log("this.props in layout", this.prps);
+        return (
+            <Auxillary>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <ToolBar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <main className={classes.content}>{this.props.children}</main>
+            </Auxillary>
+        );
+    }
+}
